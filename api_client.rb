4bc3d885aaf34eab7c71fdde69d86796effb131b
IslamRustamov <islamrustamov@rambler.ru>
2020-01-05 01:04:00 +0300

    Add CRUD operations for posts (not all of them)

--- api_client.rb
+++ api_client.rb
@@ -5,10 +5,14 @@ client = Faraday.new(url: 'http://localhost:3000') do |config|
   config.adapter  Faraday.default_adapter
 end
 
-response = client.post do |req|
-  req.url '/api/v1/users'
+client = Faraday.new(url: 'http://localhost:3000') do |config|
+  config.adapter  Faraday.default_adapter
+  config.token_auth('92b48dbb87c834a26afe2b53c80e233c')
+end
+
+response = client.delete do |req|
+  req.url '/api/v1/posts/6'
   req.headers['Content-Type'] = 'application/json'
-  req.body = '{ "user": {"name": "test user"} }'
 end
 
 puts Oj.load(response.body)
